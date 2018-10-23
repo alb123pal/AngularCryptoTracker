cbceaa4f873e9e8e2d7c67d2f1172d9740933c94
albert <albertpalka1995@gmail.com>
2018-10-23 16:46:07 +0200

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CoinListComponent } from './coin-list/coin-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CoinListComponent,
   ],
   imports: [
     BrowserModule
